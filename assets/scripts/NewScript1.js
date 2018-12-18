3f94c9ce0945129d2455f916e72f47725377d388
AthanGriv <43947962+AthanGriv@users.noreply.github.com>
2018-12-18 20:27:45 +0200

    Update NewScript1.js

--- assets/scripts/NewScript1.js
+++ assets/scripts/NewScript1.js
@@ -1,11 +1,17 @@
 
-nv.addGraph(function() {
+v.addGraph(function() {
   var chart = nv.models.pieChart()
       .x(function(d) { return d.label })
       .y(function(d) { return d.value })
-      .showLabels(true);
+      .showLabels(true)     //Display pie labels
+      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+      .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+      .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+  .growOnHover(false)
+      ;
 
-    d3.select("#chart svg")
+    d3.select("#chart2 svg")
         .datum(exampleData())
         .transition().duration(350)
         .call(chart);
